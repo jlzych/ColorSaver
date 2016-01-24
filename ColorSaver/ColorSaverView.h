0d2af9730ae91161feaf0da54ca73e48211822e7
Jeff Zych <jlzych@gmail.com>
2016-01-24 12:12:57 -0800

    Initial Commit

---
+++ ColorSaver/ColorSaverView.h
@@ -0,0 +1,13 @@
+//
+//  ColorSaverView.h
+//  ColorSaver
+//
+//  Created by Jeff Zych on 1/24/16.
+//  Copyright © 2016 Chaos Theory Studios. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ColorSaverView : ScreenSaverView
+
+@end
---
+++ ColorSaver/ColorSaverView.m
@@ -0,0 +1,52 @@
+//
+//  ColorSaverView.m
+//  ColorSaver
+//
+//  Created by Jeff Zych on 1/24/16.
+//  Copyright © 2016 Chaos Theory Studios. All rights reserved.
+//
+
+#import "ColorSaverView.h"
+
+@implementation ColorSaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
